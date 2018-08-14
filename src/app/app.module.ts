64652e026c2dc13bbb3a5bc945ea0f09e94fb5b6
Stephen Pugliese <stephen.pugliese@outlook.com>
2018-08-13 20:09:21 -0400

    Login page made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+const appRoutes: Routes = [{
+	path: "admin",
+	component: AppComponent
+},{
+	path: "",
+	redirectTo: "/admin",
+	pathMatch: "full"
+}];
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot(appRoutes,{enableTracing: true})
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
