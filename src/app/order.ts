e4c7891e41ffb921b2b776f6226e1f11c930ceb8
Stephen Pugliese <stephen.pugliese@outlook.com>
2018-09-29 15:34:48 -0400

    #8
    Added order interface to new orders + redid some code

---
+++ src/app/order.ts
@@ -0,0 +1,10 @@
+export interface Order {
+	lender: string;
+	loan_amount: number;
+	purchase_price: number;
+	property_address: string;
+	corporation: string;
+	reference_number: string;
+	buyers: any;
+	sellers: any;
+}
