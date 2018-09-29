9fc8ee1d53f773b936aafc86370d0b6833575516
Stephen Pugliese <stephen.pugliese@outlook.com>
2018-09-29 14:57:29 -0400

    #8
    Moved routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
 import { HttpClientModule, HttpHandler } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from "./app.component";
@@ -22,33 +21,7 @@ import { CookieService } from 'ngx-cookie-service';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptor } from './token.interceptor';
 import { MarketingComponent } from './marketing/marketing.component';
-
-const appRoutes: Routes = [{
-	path: "admin",
-	component: LoginComponent
-},{
-	path: "admin/dashboard",
-	component: DashboardComponent,
-	canActivate: [AuthGuard]
-},{
-	path: "admin/dashboard/order/:orderid",
-	component: OrderComponent,
-	canActivate: [AuthGuard]
-},{
-	path: "admin/dashboard/create/order",
-	component: OrderNewComponent,
-	canActivate: [AuthGuard]
-},{
-	path: "admin/users",
-	component: UserComponent,
-	canActivate: [AuthGuard]
-},{
-	path: "admin/user/forgot",
-	component: ForgotComponent
-},{
-	path: "",
-	component: MarketingComponent
-}];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
 	declarations: [
@@ -66,11 +39,11 @@ const appRoutes: Routes = [{
 	],
 	imports: [
 		BrowserModule,
-		RouterModule.forRoot(appRoutes,{enableTracing: false}),
 		HttpClientModule,
 		FormsModule,
 		NgbModule,
-		NgxSpinnerModule
+		NgxSpinnerModule,
+		AppRoutingModule
 	],
 	providers: [CookieService, {
 		provide: HTTP_INTERCEPTORS,
