f393b689167a0c7ac62113b9e18bb47c49a58c76
Stephen Pugliese <stephen.pugliese@outlook.com>
2019-05-22 20:34:42 -0400

    #36
    Linting

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,61 +1,61 @@
-import { Component, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import * as lodash from "lodash";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as lodash from 'lodash';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
-	selector: "app-user",
-	templateUrl: "./user.component.html",
-	styleUrls: ["./user.component.css"]
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-	public users: any = [];
-	constructor(private http: HttpClient, private spinner: NgxSpinnerService){
-	}
-	ngOnInit() {
-		this.http.get("/api/users/").subscribe((val) => {
-			this.users = val;
-			console.log("GET call successful value returned in body", val);
-		}, response => {
-			console.log("GET call in error", response);
-		}, () => {
-			console.log("The GET observable is now completed.");
-		});
-	}
-	userApprove(id){
-		if (confirm("Are you sure you want to approve this account?")){
-			this.spinner.show();
-			this.http.put("/api/user",{
-				id: id,
-				approved: true
-			}).subscribe((val) => {
-				console.log("PUT call successful value returned in body", val);
-				let user = lodash.find(this.users, {
-					id: id
-				});
-				setTimeout(() => {
-					user.approved = 1;
-					this.spinner.hide();
-				}, 2000);
-			}, response => {
-				console.log("PUT call in error", response)
-			}, () => {
-				console.log("The PUT observable is now completed.");
-			});
-		}
-	}
-	resetPassword(id){
-		this.http.post("/api/user/forgot", {
-			id: id
-		}).subscribe((val) => {
-			console.log("GET call successful value returned in body", val);
-			if(val){
-				alert("Password reset email sent");
-			}
-		}, response => {
-			console.log("GET call in error", response);
-		}, () => {
-			console.log("The GET observable is now completed.");
-		});
-	}
+    public users: any = [];
+    constructor(private http: HttpClient, private spinner: NgxSpinnerService) {
+    }
+    ngOnInit() {
+        this.http.get('/api/users/').subscribe((val) => {
+            this.users = val;
+            console.log('GET call successful value returned in body', val);
+        }, response => {
+            console.log('GET call in error', response);
+        }, () => {
+            console.log('The GET observable is now completed.');
+        });
+    }
+    userApprove(id) {
+        if (confirm('Are you sure you want to approve this account?')) {
+            this.spinner.show();
+            this.http.put('/api/user', {
+                id: id,
+                approved: true
+            }).subscribe((val) => {
+                console.log('PUT call successful value returned in body', val);
+                const user = lodash.find(this.users, {
+                    id: id
+                });
+                setTimeout(() => {
+                    user.approved = 1;
+                    this.spinner.hide();
+                }, 2000);
+            }, response => {
+                console.log('PUT call in error', response);
+            }, () => {
+                console.log('The PUT observable is now completed.');
+            });
+        }
+    }
+    resetPassword(id) {
+        this.http.post('/api/user/forgot', {
+            id: id
+        }).subscribe((val) => {
+            console.log('GET call successful value returned in body', val);
+            if (val) {
+                alert('Password reset email sent');
+            }
+        }, response => {
+            console.log('GET call in error', response);
+        }, () => {
+            console.log('The GET observable is now completed.');
+        });
+    }
 }
