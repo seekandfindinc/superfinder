adbd3e54a5b8b8a06f23f7f7fff5346ce9023926
Stephen Pugliese <stephen.pugliese@outlook.com>
2019-06-11 23:59:14 -0400

    #41
    Now some real fun

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-npm -v >> /home/ubuntu/output.log
-# sudo pm2 stop all > /home/ubuntu/output.log
-
-# sudo pm2 delete all >> /home/ubuntu/output.log
+sudo pm2 stop all >> /home/ubuntu/output.log
+sudo pm2 delete all >> /home/ubuntu/output.log
